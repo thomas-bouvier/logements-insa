732628710d056f0b2b1ee18d130dc368b5abe82d
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-04-17 13:33:46 +0200

    Initial load

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Logements INSA
+
+Cette application te permettra de trouver un logement facilement !
---
+++ resources/views/front/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.front')
+
+@section('content')
+
+<div class="container">
+  <p>Flux de posts</p>
+</div>
+
+@endsection
