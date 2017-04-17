d50958009802a73824a1e89b6d5b7f1fce94edcd
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-04-17 13:45:29 +0200

    Installed phpCAS

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Logements',
+    'name' => 'Logements INSA',
 
     /*
     |--------------------------------------------------------------------------
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * phpCAS
+         */
+        Subfission\Cas\CasServiceProvider::class,
     ],
 
     /*
@@ -225,7 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Cas' => Subfission\Cas\Facades\Cas::class,
     ],
 
 ];
