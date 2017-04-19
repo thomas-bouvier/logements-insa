79d5083240c5dae1eb85df54529ae992049cfc6d
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-04-20 01:44:41 +0200

    Fixed wrong var name

--- resources/views/bids/edit.blade.php
+++ resources/views/bids/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="container">
-  <h4>Éditer l'offre « {{ bid->name }} »</h4>
+  <h4>Éditer l'annonce « {{ $bid->name }} »</h4>
 
   @include('bids.form', ['action' => 'update'])
 </div>
