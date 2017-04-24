790e8636212047e3e9c3333d907221086cbf0503
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-04-24 19:01:18 +0200

    Added picture processing

--- app/Bid.php
+++ app/Bid.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class Bid extends Model
 {
@@ -13,5 +14,26 @@ class Bid extends Model
     public function type()
     {
       return $this->belongsTo('App\Type');
+    }
+
+    public function setPictureAttribute($picture)
+    {
+        if (is_object($picture) && $picture->isValid())
+        {
+            self::saved(function($instance) use ($picture)
+            {
+                $picture = $picture->move($instance->getPictureDirectory(), $instance->id . '.' . $picture->getClientOriginalExtension());
+
+                Image::make($picture)->fit(360, 200)->save($instance->getPictureDirectory() . '/' . $instance->id . '_thumb.jpg');
+                Image::make($picture)->fit(940, 530)->save($instance->getPictureDirectory() . '/' . $instance->id . '_large.jpg');
+            });
+
+            $this->attributes['picture'] = $picture->getClientOriginalExtension();
+        }
+    }
+
+    public function getPictureDirectory()
+    {
+        return 'img/pictures/' . ceil($this->id / 1000);
     }
 }
--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -7,60 +7,60 @@ use App\Bid;
 
 class BidController extends Controller
 {
-  public function __construct()
-  {
-    $this->middleware('owner', ['except' => ['index', 'store', 'create']]);
-  }
+    public function __construct()
+    {
+        $this->middleware('owner', ['except' => ['index', 'store', 'create']]);
+    }
 
-  public function getResource($id)
-  {
-    return Bid::findOrFail($id);
-  }
+    public function getResource($id)
+    {
+        return Bid::findOrFail($id);
+    }
 
-  public function index()
-  {
-    $bids = Bid::where('user_id', cas()->user())->get();
-    $bids->load('type');
+    public function index()
+    {
+        $bids = Bid::where('user_id', cas()->user())->get();
+        $bids->load('type');
 
-    return view('bids.index', compact('bids'));
-  }
+        return view('bids.index', compact('bids'));
+    }
 
-  public function create()
-  {
-    $bid = new Bid();
+    public function create()
+    {
+        $bid = new Bid();
 
-    return view('bids.create', compact('bid'));
-  }
+        return view('bids.create', compact('bid'));
+    }
 
-  public function store(BidRequest $request)
-  {
-    $data = $request->all();
-    $data['user_id'] = cas()->user();
+    public function store(BidRequest $request)
+    {
+        $data = $request->all();
+        $data['user_id'] = cas()->user();
 
-    Bid::create($data);
+        Bid::create($data);
 
-    return redirect(action('BidController@index'))->with('success', "Votre annonce a bien été créée.");
-  }
+        return redirect(action('BidController@index'))->with('success', "Votre annonce a bien été créée.");
+    }
 
-  public function edit($bid)
-  {
-    return view('bids.edit', compact('bid'));
-  }
+    public function edit($bid)
+    {
+        return view('bids.edit', compact('bid'));
+    }
 
-  public function update($bid, BidRequest $request)
-  {
-    $data = $request->all();
-    $data['user_id'] = cas()->user();
+    public function update($bid, BidRequest $request)
+    {
+        $data = $request->all();
+        $data['user_id'] = cas()->user();
 
-    $bid->update($data);
+        $bid->update($data);
 
-    return redirect(action('BidController@index'))->with('success', "Votre annonce a bien été mise à jour.");
-  }
+        return redirect(action('BidController@index'))->with('success', "Votre annonce a bien été mise à jour.");
+    }
 
-  public function destroy($bid)
-  {
-    $bid->delete();
+    public function destroy($bid)
+    {
+        $bid->delete();
 
-    return redirect(action('BidController@index'))->with('success', "Votre annonce a bien été supprimée.");
-  }
+        return redirect(action('BidController@index'))->with('success', "Votre annonce a bien été supprimée.");
+    }
 }
