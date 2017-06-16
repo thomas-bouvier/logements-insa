432939a4fdb2760bdc9fc7f2d22bcabe2f3d7a0f
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-06-17 01:12:27 +0200

    Added a message if there is no bid

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,57 +3,61 @@
 @section('content')
 
 <div class="container">
-  @foreach ($bids as $bid)
-    <article class="bid">
-      <div class="bid-title">
-        @if (App\User::where('login', cas()->user())->first()->id == $bid->user_id)
-          <h4>{{ $bid->name }}</h4>
-        @else
-          <h4>{{ $bid->name }} <small>par {{ App\User::where('id', $bid->user_id)->first()->login }}</small></h4>
-        @endif
-      </div>
+    @if ($bids != null)
+        <p>Il n'y a pas d'annonce pour le moment :(</p>
+    @else
+        @foreach ($bids as $bid)
+            <article class="bid">
+                <div class="bid-title">
+                    @if (App\User::where('login', cas()->user())->first()->id == $bid->user_id)
+                        <h4>{{ $bid->name }}</h4>
+                    @else
+                        <h4>{{ $bid->name }} <small>par {{ App\User::where('id', $bid->user_id)->first()->login }}</small></h4>
+                    @endif
+                </div>
 
-      <div class="row" style="margin-bottom: 15px;">
-        <div class="col-md-9">
-          <img src="{{ $bid->photo('large', 1) }}" class="img-responsive" alt="{{ $bid->name }}">
+                <div class="row" style="margin-bottom: 15px;">
+                    <div class="col-md-9">
+                        <img src="{{ $bid->photo('large', 1) }}" class="img-responsive" alt="{{ $bid->name }}">
+                    </div>
+
+                    <div class="col-md-3">
+                        <p class="author">Annonce publiée le {{ $bid->created_at->formatLocalized('%d %B %Y') }} par {{ App\User::where('id', $bid->user_id)->first()->login }} ({{ $bid->created_at->diffForHumans() }}).</p>
+                        <h5 class="bid-details-title">Caractéristiques</h5>
+                        <p class="bid-details">
+                            <ul>
+                                <li>
+                                    Type de bien : {{ $bid->type->name }}
+                                </li>
+
+                                <li>
+                                    Montant du loyer : {{ $bid->rental }} €
+                                </li>
+
+                                <li>
+                                    Surface : environ {{ $bid->ground }} m<sup>2</sup>
+                                </li>
+
+                                <li>
+                                    Localisation : {{ $bid->district }}
+                                </li>
+                            </ul>
+                        </div>
+                    </p>
+                </div>
+
+                <a class="btn btn-primary" href="{{ action('BidController@show', $bid) }}">Voir</a>
+                @if (App\User::where('login', cas()->user())->first()->id == $bid->user_id)
+                    <a class="btn btn-default" href="{{ action('BidController@edit', $bid) }}">Éditer</a>
+                    <a class="btn btn-danger" href="{{ action('BidController@destroy', $bid) }}" data-method="delete" data-confirm="Voulez-vous vraiment supprimer l'annonce « {{ $bid->name }} » ?">Supprimer</a>
+                @endif
+            </article>
+        @endforeach
+
+        <div class="pagination">
+            {{ $bids->render() }}
         </div>
-
-        <div class="col-md-3">
-          <p class="author">Annonce publiée le {{ $bid->created_at->formatLocalized('%d %B %Y') }} par {{ App\User::where('id', $bid->user_id)->first()->login }} ({{ $bid->created_at->diffForHumans() }}).</p>
-          <h5 class="bid-details-title">Caractéristiques</h5>
-          <p class="bid-details">
-            <ul>
-              <li>
-                Type de bien : {{ $bid->type->name }}
-              </li>
-
-              <li>
-                Montant du loyer : {{ $bid->rental }} €
-              </li>
-
-              <li>
-                Surface : environ {{ $bid->ground }} m<sup>2</sup>
-              </li>
-
-              <li>
-                Localisation : {{ $bid->district }}
-              </li>
-            </ul>
-          </div>
-        </p>
-      </div>
-
-      <a class="btn btn-primary" href="{{ action('BidController@show', $bid) }}">Voir</a>
-      @if (App\User::where('login', cas()->user())->first()->id == $bid->user_id)
-        <a class="btn btn-default" href="{{ action('BidController@edit', $bid) }}">Éditer</a>
-        <a class="btn btn-danger" href="{{ action('BidController@destroy', $bid) }}" data-method="delete" data-confirm="Voulez-vous vraiment supprimer l'annonce « {{ $bid->name }} » ?">Supprimer</a>
-      @endif
-    </article>
-  @endforeach
-
-  <div class="pagination">
-    {{ $bids->render() }}
-  </div>
+    @endif
 </div>
 
 @endsection
