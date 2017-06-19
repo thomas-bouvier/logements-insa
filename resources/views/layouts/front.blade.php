cd62b7796a5d3a5b27d8e32fd0f5c29e995343c5
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-06-19 15:27:34 +0200

    Added Google Analytics

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -22,6 +22,27 @@
             'csrfToken' => csrf_token(),
         ]) !!};
     </script>
+
+    @if (App\User::where('login', cas()->user())->first()->role != 'admin')
+    <script>
+        if (window.location.hostname != 'localhost' && window.location.hostname != '127.0.0.1') {
+            (function(i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                    m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-101248635-1', 'auto');
+            ga('send', 'pageview');
+        }
+    </script>
+    @endif
 </head>
 <body>
     <header>
